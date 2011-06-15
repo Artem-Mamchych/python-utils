19fc9abd70db58e1d2a9e79d5ac5fcb6ff4e1ca6
tqfp <dphoto.nx.@gmail.com>
2011-06-15 12:29:08 +0300

    now list in json format

--- src/flist.py
+++ src/flist.py
@@ -4,35 +4,30 @@ Creates filelist of dropbox public folder
 Created on Jun 14, 2011
 '''
 import os
+import json
 
 dropbox_folder_location = "d:\\Dropbox\\Public"
 dropbox_baseurl = "http://dl.dropbox.com/u/YOUR_ID"
-filelist_filename = "filelist.py"
-filelist_startstring = "filelist = {"
-identation = "\n\t"
-filelist_lastentity = "'get.py':'http://url'"
-filelist_endstring = identation + filelist_lastentity + "\n}"
-filelist_separator = ","
+filelist_filename = "filelist.json"
 files_count = 0
 
 def scan_dirs():
+    filelist = {}
     file = open(filelist_filename, 'w')
-    file.write(filelist_startstring)
 
     for root, dirs, files in os.walk(dropbox_folder_location):
         for name in files:
             filepath = os.path.join(root, name)
             url = filepath.replace(dropbox_folder_location, dropbox_baseurl)
             url = url.replace("\\", "/")
-            print "File: %s \t\t\tUrl: %s" % (name, url)
-            buffer = identation + "'%s': '%s'," % (name, url)
-            file.write(buffer)
+            print "File: %s \t\tUrl: %s" % (name, url)
+            if not filelist.has_key(name):
+                filelist[name] = [url]
 
-    file.write(filelist_endstring)
+    json.dump(filelist, file)
     return 0
-    
+
 def main():
-    #for arg in sys.argv[1:]:
     scan_dirs()
     return 0
 
